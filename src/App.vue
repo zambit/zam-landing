c9d76ae874145f7a0ea1d10be0efc2c5e578ed97
Nikich <nikich@zam.me>
2018-11-30 18:04:28 +0300

    feat: show landing version in console on mount

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,16 @@
 import navbar from '@/components/layout/navbar';
 import vFooter from '@/components/layout/footer';
 
+import pkg from '../package.json';
+
 export default {
   name: 'App',
   components: {
     navbar,
     vFooter,
+  },
+  mounted() {
+    console.log(pkg.version); // eslint-disable-line
   },
 };
 </script>
